1d2c12e13340bb68a7b18b23a7f26d6658af57b9
marknesh <markmunene.nesh@gmail.com>
2020-03-09 13:01:23 +0300

    create search functionality to search for users

--- gram/tests.py
+++ gram/tests.py
@@ -5,7 +5,7 @@ from .models import Image
 class ImageTest(TestCase):
 
     def setUp(self):
-        self.image=Image(title="fff",caption="VV",pub_date="DD",image="mm",comments="FF",profile=3,comments_number=3)
+        self.image=Image(title="fff",caption="VV",pub_date="DD",image="mm",comments="FF",profile=3,comments_number=3,likes=4)
 
 
     def test_instance(self):
